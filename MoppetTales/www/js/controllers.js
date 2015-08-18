b03d7e055fc894baaa94c381a5ad8e64ed8d8ddc
camberos <miguel.camberos@gmail.com>
2015-08-18 16:58:01 +1000

    Added Facebook Profile

--- MoppetTales/www/js/controllers.js
+++ MoppetTales/www/js/controllers.js
@@ -78,6 +78,19 @@ angular.module('starter.controllers', ['ngOpenFB'])
     
 })
 
+.controller('ProfileCtrl', function ($scope, ngFB) {
+    ngFB.api({
+        path: '/me',
+        params: {fields: 'id,name'}
+    }).then(
+        function (user) {
+            $scope.user = user;
+        },
+        function (error) {
+            alert('Facebook error: ' + error.error_description);
+        });
+})
+
 .controller('PlaylistCtrl', function($scope, $stateParams, $http) {
     
 });
