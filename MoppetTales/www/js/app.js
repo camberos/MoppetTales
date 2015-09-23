28435c7de837261152b9b77eb14220be38330c17
camberos <miguel.camberos@gmail.com>
2015-09-23 11:53:04 +1000

    iOS9 UIWebView Patch for Angular

--- MoppetTales/www/js/app.js
+++ MoppetTales/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'ngOpenFB', 'ionic.ion.imageCacheFactory','ionicLazyLoad'])
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'ngOpenFB', 'ionic.ion.imageCacheFactory','ionicLazyLoad','ngIOS9UIWebViewPatch'])
 
 .run(function($ionicPlatform, ngFB) {
   ngFB.init({appId: '1384279435212091'});
